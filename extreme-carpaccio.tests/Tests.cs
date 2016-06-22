d045ff61e3a41a8190e4d645c24a0c2ff4e228e1
RPoulalier <remy.poulalier@etu.univ-lyon1.fr>
2016-06-22 08:55:15 +0200

    Mise en route serveur Création du projet de test Ecriture de tests de vérification
    Signed-off-by: RPoulalier <remy.poulalier@etu.univ-lyon1.fr>

---
+++ extreme-carpaccio.tests/Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace extreme_carpaccio.tests
+    
+{
+    public class Tests
+    {
+        [Test]
+        public void calculer_la_note()
+        {
+            Assert.IsFalse(false);
+            Assert.That(true,Is.True);
+        }
+
+        [Test]
+        public void ajouter_un_produit()
+        {
+            var toto = 3;
+            Assert.That(toto,Is.EqualTo(3));
+        }
+    }
+}
